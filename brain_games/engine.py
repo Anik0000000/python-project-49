038d89877ed4e057658922b28a8e084d9cde374a
anik0000000 <idozmor@gmail.com>
2025-08-07 12:28:57 +0500

    Add new game GCD

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -6,26 +6,14 @@ def run_game(game):
 
     for _ in range(3):
         question, correct_answer = game.generate_round()
-        
         print(f"Question: {question}")
-        user_answer = input("Your answer: ").strip().lower()  
-        if game.__name__ == 'brain_games.games.calc':
-            if not user_answer.lstrip('-').isdigit():
-                print("❌ Please enter a number! Try again.")
-                    continue
-                break
-                
-        elif game.__name__ == 'brain_games.games.even':
-            if user_answer.lower() not in ('yes', 'no'):
-                print("❌ Please answer 'yes' or 'no'! Try again.")
-                    continue
-                break
+        user_answer = input("Your answer: ").strip()
 
         if user_answer == correct_answer:
             print("Correct!")
         else:
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")  
+            print(f"Let's try again, {name}!")
             return
 
     print(f"Congratulations, {name}!")
--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,14 +6,11 @@ def generate_round():
     a = random.randint(1, 50)
     b = random.randint(1, 50)
     operator = random.choice(['+', '-', '*'])
-
+    
     match operator:
-        case '+':
-            correct_answer = str(a + b)
-        case '-':
-            correct_answer = str(a - b)
-        case '*':
-            correct_answer = str(a * b)
-
+        case '+': correct_answer = str(a + b)
+        case '-': correct_answer = str(a - b)
+        case '*': correct_answer = str(a * b)
+    
     question = f"{a} {operator} {b}"
     return question, correct_answer
